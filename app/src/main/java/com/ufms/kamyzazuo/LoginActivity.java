e0d8d9dfa079c8e9df37d8faa9abf077362d98b1
Paulo Roberto <paulo1060@gmail.com>
2021-04-21 21:53:03 -0400

    Tela de login

--- app/src/main/java/com/ufms/kamyzazuo/LoginActivity.java
+++ app/src/main/java/com/ufms/kamyzazuo/LoginActivity.java
@@ -1,27 +1,63 @@
 package com.ufms.kamyzazuo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     Button bttlogin;
+    EditText email,senha;
+    String emailS,senhaS;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         bttlogin=findViewById(R.id.bttlogin);
+        email=findViewById(R.id.edttxtemail);
+        senha=findViewById(R.id.edtTxtTsenha);
+
+        mAuth= FirebaseAuth.getInstance();
 
         bttlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it=new Intent(LoginActivity.this,MenuDoFunc.class);
-                startActivity(it);
-
+                emailS=email.getText().toString().trim();
+                senhaS=senha.getText().toString();
+                if(TextUtils.isEmpty(emailS)){
+                    email.setError("Email é necessaria");
+                    return;
+                }
+                if(TextUtils.isEmpty(senhaS)){
+                    email.setError("Senha é necessaria");
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(emailS,senhaS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login concluido com sucesso", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MenuDoFunc.class));
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "Erro ao realizar o login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
